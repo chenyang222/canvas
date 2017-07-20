2eecb985787d5b7e712358a7e4eaab6666d61877
chenyang222 <351424947@qq.com>
2017-07-20 15:43:31 +0800

    20170720

---
+++ mycanvasfn/ranchy_canvasfn.js
@@ -0,0 +1,53 @@
+
+//Ranchy canvas function
+
+//动画兼容
+if(!window.requestAnimationFrame){
+    window.requestAnimationFrame =(window.webkitRequestAnimationFrame||
+                                   window.mozRequestAnimationFrame||
+                                   window.oRequestAnimationFrame||
+                                   window.msRequestAnimationFrame||
+                                  function(callback){
+                                       return window.setTimeout(callback,1000/60); 
+                                 });
+    
+}
+if (!window.cancelAnimationFrame) {
+  window.cancelAnimationFrame = (window.cancelRequestAnimationFrame ||
+                                 window.webkitCancelAnimationFrame || window.webkitCancelRequestAnimationFrame ||
+                                 window.mozCancelAnimationFrame || window.mozCancelRequestAnimationFrame ||
+                                 window.msCancelAnimationFrame || window.msCancelRequestAnimationFrame ||
+                                 window.oCancelAnimationFrame || window.oCancelRequestAnimationFrame ||
+                                 window.clearTimeout);
+}
+
+window.ranchy = {};
+
+
+//鼠标在画布中位置捕获
+
+window.ranchy.captureMouse = function(e){
+	
+        var mouse = {x:0,y:0};
+        
+        e.addEventListener('mousemove',function(event){
+            var x,y;
+            if(event.pageX||event.pageY){
+                x = event.pageX;
+                y = event.pageY;
+            }else{
+                x = event.clientX + document.body.scrollLeft +document.documentElement.scrollLeft;
+                y = event.clientY + document.body.scrollTop +document.documentElement.scrollTop;
+            }
+            
+            x -= e.offsetLeft;
+            y -= e.offsetTop;
+            
+            mouse.x = x;
+            mouse.y = y;          
+            
+        },false);
+
+         return mouse;  	
+	
+}
